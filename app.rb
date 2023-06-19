28de00bd3a2f03acc690972180d7460c437057e8
kaltag <alexx30rus@gmail.com>
2023-06-19 15:24:11 +0400

    delete ninja

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ class App
   def call(env)
     request = Rack::Request.new(env)
     time = Format.new(request.params)
+    time.call!
     response = if time.success?
                  Rack::Response.new(time.body, 200, { 'Content-Type' => 'text/plain' })
                else
